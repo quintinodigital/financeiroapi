cf0f72488b8fe16cf669e2a45f9d1c81f3f112f2
Dysprosium <dysprosium@outlook.com.br>
2022-09-29 14:14:59 -0300

    Estrutura Básica do Projeto

---
+++ financeiroapi/src/test/java/digital/quintino/financeiroapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.quintino.financeiroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
